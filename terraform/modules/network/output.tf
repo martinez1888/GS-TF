0f06cbae472344e48f9344295b0377d7eb45350b
martinez1888 <78212114+martinez1888@users.noreply.github.com>
2022-11-09 21:21:05 -0300

    Create output.tf

---
+++ terraform/modules/network/output.tf
@@ -0,0 +1,37 @@
+# NETWORK OUTPUT TO BE REUSED
+
+output "vpc_id" {
+    value = "${aws_vpc.vpc.id}"
+}
+
+output "vpc_sg_pub_id" {
+    value = "${aws_security_group.vpc_sg_pub.id}"
+}
+
+output "vpc_sg_priv_id" {
+    value = "${aws_security_group.vpc_sg_priv.id}"
+}
+
+output "vpc_sn_az1_pub_id" {
+    value = "${aws_subnet.sn_az1_pub.id}"
+}
+
+output "vpc_sn_az2_pub_id" {
+    value = "${aws_subnet.sn_az2_pub.id}"
+}
+
+output "vpc_sn_az1_priv1_id" {
+    value = "${aws_subnet.sn_az1_priv1.id}"
+}
+
+output "vpc_sn_az2_priv1_id" {
+    value = "${aws_subnet.sn_az2_priv1.id}"
+}
+
+output "vpc_sn_az1_priv2_id" {
+    value = "${aws_subnet.sn_az1_priv2.id}"
+}
+
+output "vpc_sn_az2_priv2_id" {
+    value = "${aws_subnet.sn_az2_priv1.id}"
+}
